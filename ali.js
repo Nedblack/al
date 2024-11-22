749c27d1615ca002d0cbe167c27dba35b759e068
Nedblack <108587461+Nedblack@users.noreply.github.com>
2024-11-22 19:34:42 +0300

    Update ali.js

--- ali.js
+++ ali.js
@@ -65,7 +65,7 @@ margin-top: 2px !important;
   margin-bottom: 3px !important;
   margin-top: 2px !important;
   border-radius: 0px 10px 0px 10px;
-         border: 1px solid #fff;
+         
  background-image: url(https://www2.0zz0.com/2024/11/22/15/912360466.jpg);
 background-size: 100%;
       
