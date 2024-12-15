7516efa8faaabec386c9dd50841ce0a9bdb07e82
Nedblack <108587461+Nedblack@users.noreply.github.com>
2024-12-15 11:12:36 +0300

    Update ali.js

--- ali.js
+++ ali.js
@@ -105,11 +105,11 @@ background-size: 100%;
   
 
 #users .uzr.baqer .itarr_baqer {
-   width: 77px;
-height: 77px;
-margin-top: -15px;
+   width: 80px;
+height: 80px;
+margin-top: -39px;
 cursor: pointer;
-margin-left: -14px;
+margin-left: -40px;
 }`);
 const xxxx = [ {
   'name': "baqer",
